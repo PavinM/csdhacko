f8dff042d4d5b2c315182b4741c77183f9828ec6
Pavin M <pavin.24aim@kongu.edu>
2026-02-26 23:36:39 +0530

    Update backend CORS to allow Vercel domain

--- server/index.js
+++ server/index.js
@@ -19,7 +19,13 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+        "https://kec-portal.vercel.app"
+    ],
     credentials: true
 }));
 app.use(express.json());
